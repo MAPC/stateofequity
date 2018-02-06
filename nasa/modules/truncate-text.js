7a060389408b9c2e0229804fa5e170e124307a64
Eric Youngberg <eyoungberg@mapc.org>
2018-02-06 10:09:49 -0500

    Fix foot notes for truncated paragraphs

--- nasa/modules/truncate-text.js
+++ nasa/modules/truncate-text.js
@@ -8,11 +8,44 @@ Nasa.launch('truncate-text', () => {
   const url = '/report#';
 
   const blocks = Array.from(document.querySelectorAll('*[data-truncate]'));
+  const notes = Array.from(document.querySelectorAll('.footnotes'))
+                     .map(list => Array.from(list.querySelectorAll('li')))
+                     .reduce((a,b) => a.concat(b));
 
 
   /**
    * State
    */
+
+  const removeFootnotes = p => {
+    const footnotes = Array.from(p.querySelectorAll('a.footnote'));
+    let list = null;
+
+    if (footnotes.length > 0) {
+      footnotes.forEach(footnote => {
+        let ref = null;
+
+        notes.some(note => {
+          if (note.id === footnote.href.split('#')[1]) {
+            ref = note;
+            return true; 
+          }
+        });
+        
+        if (ref) {
+          list = ref.parentNode;
+          list.removeChild(ref);
+        }
+      });
+
+      if (list) {
+        if (list.querySelectorAll('li').length === 0) {
+          list.parentNode.removeChild(list);
+        }
+      }
+    }
+  };
+
 
   blocks.forEach(block => {
     const paragraphs = Array.from(block.querySelectorAll('p'));
@@ -20,12 +53,15 @@ Nasa.launch('truncate-text', () => {
 
     paragraphs.forEach(paragraph => {
       if (wordCount >= wordLimit) {
+        removeFootnotes(paragraph);
         paragraph.parentNode.removeChild(paragraph);
       }
       else {
         const words = paragraph.innerText.split(' ');
 
         if ((wordCount + words.length) >= wordLimit) {
+          removeFootnotes(paragraph);
+
           paragraph.innerText = words.slice(0, (wordLimit - wordCount)).join(' ') + ' ...';
 
           const reportAnchor = document.createElement('a');
