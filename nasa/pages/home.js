fc38099b3dc0651e8c43411b449e3494a165a8c2
Eric Youngberg <eyoungberg@mapc.org>
2018-02-06 20:04:15 -0500

    Create download link for pdf

--- nasa/pages/home.js
+++ nasa/pages/home.js
@@ -1,5 +1,23 @@
 Nasa.launch('home-page', () => {
 
+  /**
+   * Setup
+   */
+
   Nasa.land('tabs')('.disparities');
+  
+  const downloads = Array.from(document.querySelectorAll('*[data-download]'));
+
+  
+  /**
+   * State
+   */
+
+  downloads.forEach(download => {
+    download.addEventListener('click', e => {
+      e.preventDefault();
+    });
+  });
+
 
 });
